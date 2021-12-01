d04849fbbf83665a9639bfe21f613d565846b9f4
Samitier <blai.samitier@gmail.com>
2021-12-01 22:24:43 +0100

    day 1

---
+++ src/01.1/01.1.ts
@@ -0,0 +1,15 @@
+// https://adventofcode.com/2021/day/1
+
+import input from './input.json'
+
+function sonarSweep(values: number[]) {
+  return values.reduce((previous, current, i) => {
+    if (i !== 0 && current > values[i - 1]) return previous + 1
+    return previous
+  }, 0)
+}
+
+// Example
+console.log(sonarSweep([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]))
+// Solution
+console.log(sonarSweep(input))
